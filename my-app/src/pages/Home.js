2f158ae181fd9ce1343db7fce34d1695710c9c54
onifernando1 <onifernando1@gmail.com>
2023-06-02 17:01:41 +0100

    copy and paste logic to display image of each appartment in home, add classNames

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -5,17 +5,25 @@ import { useState } from "react";
 
 function Home() {
   const [appartmentArray, setAppartmentArray] = useState([
-    <Appartment image="house1" />,
-    <Appartment image="house2" />,
-    <Appartment image="house3" />,
-    <Appartment image="house4" />,
+    <Appartment image="house1" title="House 1" />,
+    <Appartment image="house2" title="House 2" />,
+    <Appartment image="house3" title="House 3" />,
+    <Appartment image="house4" title="House 4" />,
   ]);
 
   return (
     <div>
-      <div>
+      <div className="appartmentContainer">
         {appartmentArray.map((appartment) => {
-          return <div>{appartment.props.image}</div>;
+          return (
+            <div className="individualAppartment">
+              <img
+                src={require(`../assets/images/${appartment.props.image}.jpg`)}
+              ></img>
+              <div>{appartment.props.image}</div>
+              <div>{appartment.props.image}</div>
+            </div>
+          );
         })}
       </div>
     </div>
