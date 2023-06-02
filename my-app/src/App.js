a4e4ac610b2bf9737d8d5350f855d038a0bebb04
onifernando1 <onifernando1@gmail.com>
2023-06-02 16:09:26 +0100

    create basic navbar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,33 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Appartment from "./pages/Appartment";
 import Basket from "./pages/Basket";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/appartment" element={<Appartment />} />
-      <Route path="/basket" element={<Basket />} />
-    </Routes>
+    <div>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/appartment">Appartment</Link>
+            </li>
+            <li>
+              <Link to="/basket">Basket</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/appartment" element={<Appartment />} />
+        <Route path="/basket" element={<Basket />} />
+      </Routes>
+    </div>
   );
 }
 
