1ce33d8c165c1304d410ddfe4e51555eefceae4c
onifernando1 <onifernando1@gmail.com>
2023-06-02 16:40:26 +0100

    create appartment array in appartment list component using useState

--- my-app/src/pages/AppartmentList.js
+++ my-app/src/pages/AppartmentList.js
@@ -1,7 +1,9 @@
 import React from "react";
 import Appartment from "./Appartment";
+import { useState } from "react";
 
 function AppartmentList(params) {
+  const { appartmentArray, setAppartmentArray } = useState([]);
   return (
     <div>
       <Appartment image="house1" />
--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -1,7 +1,15 @@
 import React from "react";
+import Appartment from "./Appartment";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <div>
+        <Appartment image="house1" />
+      </div>
+      >
+    </div>
+  );
 }
 
 export default Home;
