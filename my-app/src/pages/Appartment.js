e23379179696ae633289da3129e31f1543efdf28
onifernando1 <onifernando1@gmail.com>
2023-06-02 16:37:02 +0100

    get image from appt props, display

--- my-app/src/pages/Appartment.js
+++ my-app/src/pages/Appartment.js
@@ -1,7 +1,12 @@
 import React from "react";
 
-function Appartment() {
-  return <div>Appt</div>;
+function Appartment(props) {
+  return (
+    <div>
+      <img src={require(`../assets/images/${props.image}.jpg`)}></img>
+      <div>This is an appartment</div>
+    </div>
+  );
 }
 
 export default Appartment;
--- my-app/src/pages/AppartmentList.js
+++ my-app/src/pages/AppartmentList.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Appartment from "./Appartment";
 
-function AppartmentList() {
-  return <div>Basket</div>;
+function AppartmentList(params) {
+  return (
+    <div>
+      <Appartment image="house1" />
+    </div>
+  );
 }
 
 export default AppartmentList;
